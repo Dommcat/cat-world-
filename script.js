179c18f25572608a72e455ab0895d9fb9291e8bf
dominick martin <dominickbmartin@gmail.com>
2022-11-17 14:49:46 -0800

    update

---
+++ script.js
@@ -0,0 +1,24 @@
+//FUNCTIONS
+
+let catName=prompt("What is your favorite cat?")
+if (catName=="ragdoll") {
+ alert ("That's a great cat!")
+
+} else {alert("too bad")
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
