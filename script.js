6b0c536c1f42febb9bcb3cd49f97ac23d5244e3f
dominick martin <dominickbmartin@gmail.com>
2022-11-17 20:16:15 -0800

    update

--- script.js
+++ script.js
@@ -1,55 +1,78 @@
 //FUNCTIONS
 
 
-
-let catName=prompt("What is your favorite cat?")
-if (catName=="Ragdoll") {
- alert ("That's a great cat!")
-
-} else {alert("too bad")
-    
+function catName() {
+    let catName = prompt("What is your favorite cat?")
+    if (catName == "ragdoll") {
+        alert ("That's a great cat!")
+    } else {
+        alert("You should try a ragdoll");
+        }
 }
 
-function likeskittens() {
-    let response = prompt("Do you like Kittens?")
+
+function displayRating() {
+
+    let output = '';
+    let rating = prompt('scale of 1-5, how many stars?');
+    for(let i = 0; i < rating; i++){
+    output += "<img class='catpic' src='catphoto.jpg' alt='cute kitty'/>";
+    }
+
+    return document.write(output);
+}
+
+
+
+// function guesscat() {
+//     let answer;
+
+//     while (answer = "ragdoll") {
+//         answer = prompt('Do you have a ragdoll cat');
+//         if (answer =="Yes") {
+//             alert("You are cool");
+//         } else {
+//             alert("You are not cool");
+//         }
+//     }
+// }
+
+
+function likecats() {
+    let response = prompt("Do you like cats?")
   
-    if (response == "yes") { // this is where I start conditional logic
-      alert("You've come to the right world!");
-      document.write("You've come to the right world!");
+    if (response == "yes") { 
+      alert("You've come to the right cat world!");
+      document.write("You've come to the right cat world!");
     } else if (response == "no") {
-      alert("What kind of person doesn't like kittens?");
+      alert("What kind of person doesn't like cats?");
       document.write("What are you doing at a cat world website?");
     } else {
       alert("I'm not sure what that means...");
-      document.write("I'm not sure what you mean with that response...");
+      document.write("Dude, I'm not sure what you mean with that response...");
     }
+}
+
+function howmanycats() {
+    let response = prompt("Guess how many cats I have?");
+    let guesses = 3;
+  for (let i=0; i<guesses; i++);
   
+    if (response == 7){
+    alert ("Your Correct!");}
+   
+    else if (response >7) {
+    alert ("too high");}
 
-
-
-function guesskitten() {
-    let answer;
-
-    while (answer != ragdoll) {
-        answer = prompt('Guess again');
-        if (answer !=7) {
-            alert('Try again!');
-        } else {
-            alert('You are correct!');
-        }
-    }
+     else if (response <7) {
+    alert("too Low");
+     }
+}
 
 
 
 
-
-
-
-
-
-// document.write()
-// alert()
-// prompt()
-// consol.log()
-// while()
-//for()
+catName()
+likecats()
+howmanycats()
+displayRating() 
